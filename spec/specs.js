438f480753f0efeaa4ebb35e59faebc80136676d
Oskar <oskarradonkimball@gmail.com>
2016-01-22 09:02:25 -0800

    Change specs to be worded correctly

--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,10 @@
 describe("pingPong", function() {
   it("will count up to a given number", function() {
-    expect(pingPong(5)).to.eql([1, 2, 3, 4, 5]);
+    expect(pingPong(5)).to.eql([1, 2, 'ping', 4, 5]);
   });
 
   it("will replace all numbers divisible by 3 with 'ping'", function(){
-    expect(pingPong(5)).to.eql([1, 2, 3, 4, 'ping']);
+    expect(pingPong(5)).to.eql([1, 2, 'ping', 4, 5]);
   });
 
 });
