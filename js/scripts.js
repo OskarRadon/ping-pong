04802f7cd7cdcabab3b65f465456379d333882f1
Oskar <oskarradonkimball@gmail.com>
2016-01-22 09:11:49 -0800

    Changed order of js conditionals
    Still not working

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,12 @@ var numbers = [];
 function pingPong (endNumber) {
   var numbers = [];     // clear the array each time function runs
   for (var i = 1; i <= endNumber; i++) {
-    if (i % 3 === 0) {
+    if (i % 15 === 0) {
+    numbers.push("pingpong");
+    } else if (i % 3 === 0) {
     numbers.push("ping");
     } else if (i % 5 === 0) {
     numbers.push("pong");
-  } else if (i % 15 === 0) {
-    numbers.push("pingpong");
     } else {
     numbers.push(i);
     };
