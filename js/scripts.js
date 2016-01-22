fb5ba0273d759f142bd780274c6a73fd7487ed4b
Oskar <oskarradonkimball@gmail.com>
2016-01-22 08:27:25 -0800

    Add js to make function return string instead of array of answers

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 var numbers = [];
+var result = "";
 function pingPong (endNumber) {
   for(var i = 1; i <= endNumber; i++){
     numbers.push(i);
+    result = numbers.join(', ');
   };
-  return numbers;
+  return result;
 };
