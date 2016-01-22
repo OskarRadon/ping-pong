dad82e55e42f18a53badedd200b4ec510ca9f981
Oskar <oskarradonkimball@gmail.com>
2016-01-22 08:32:33 -0800

    Add comments to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
-var numbers = [];
+var numbers = [];       // declare some vars
 var result = "";
+
 function pingPong (endNumber) {
-  var numbers = [];
+  var numbers = [];     // clear the array each time function runs
   for(var i = 1; i <= endNumber; i++){
     numbers.push(i);
-    result = numbers.join(', ');
+    result = numbers.join(', ');  //make array a string w/ correct formatting
   };
   return result;
 };
