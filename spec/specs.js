6f4c4e0da7ff984744101e4a9844c12638874687
Oskar <oskarradonkimball@gmail.com>
2016-01-22 09:13:39 -0800

    Fix input of final spec
    -_- durr

--- spec/specs.js
+++ spec/specs.js
@@ -12,6 +12,6 @@ describe("pingPong", function() {
   });
 
   it("will replace all numbers divisible by 15 with 'pingpong'", function(){
-    expect(pingPong(5)).to.eql([1, 2, 'ping', 4, 'pong', 'ping', 7, 8, 'ping', 'pong', 11, 'ping', 13, 14, 'pingpong']);
+    expect(pingPong(15)).to.eql([1, 2, 'ping', 4, 'pong', 'ping', 7, 8, 'ping', 'pong', 11, 'ping', 13, 14, 'pingpong']);
   });
 });
