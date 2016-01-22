cae57c19a7fbf2ff5054d3ae1178ea88f7373adb
Oskar <oskarradonkimball@gmail.com>
2016-01-22 09:07:43 -0800

    Add third failing spec for numbers divisible by 15

--- spec/specs.js
+++ spec/specs.js
@@ -10,4 +10,8 @@ describe("pingPong", function() {
   it("will replace all numbers divisible by 5 with 'pong'", function(){
     expect(pingPong(5)).to.eql([1, 2, 'ping', 4, 'pong']);
   });
+
+  it("will replace all numbers divisible by 15 with 'pingpong'", function(){
+    expect(pingPong(5)).to.eql([1, 2, 'ping', 4, 'pong', 'ping', 7, 8, 'ping', 'pong', 11, 'ping', 13, 14, 'pingpong']);
+  });
 });
