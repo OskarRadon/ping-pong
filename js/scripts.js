c747ae18872d701582030a20a447755e73f4a487
Oskar <oskarradonkimball@gmail.com>
2016-01-22 08:30:00 -0800

    Add js to clear array after each time function runs

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 var numbers = [];
 var result = "";
 function pingPong (endNumber) {
+  var numbers = [];
   for(var i = 1; i <= endNumber; i++){
     numbers.push(i);
     result = numbers.join(', ');
